03d07d516f7245a494c3b967878b63caad147774
rikardseg <70521759+rikardseg@users.noreply.github.com>
2020-12-25 18:38:45 +0100

    login form

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="style.css" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <h1>Hacker News</h1>
+  </header>
+  <ul>
+    <li><a href="#Newest posts">Newest posts</a></li>
+    <li><a href="#Create new post">Create new post</a></li>
+    <li><a href="#Most popular">Most popular</a></li>
+    <li><a href="index2.php">Login</a></li>
+    <li><a href="#Sign up">Sign up</a></li>
+  </ul>
+  <script src="script.js"></script>
+</body>
+
+</html>
---
+++ index2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="form.css" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
+  <main>
+    <h1>Login</h1>
+    <form action="index.php" method="post">
+      <label for="username">Username</label>
+      <input type="username" name="username" id="username" />
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" />
+      <button type="submit">Login</button>
+    </form>
+  </main>
+</body>
+
+</html>
