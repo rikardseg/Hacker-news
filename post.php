479ced2544cb9b50bee65a1f9c87d1e8bcdc3967
rikardseg <70521759+rikardseg@users.noreply.github.com>
2021-01-03 18:29:37 +0100

    bind specific comments to post

--- post.php
+++ post.php
@@ -9,19 +9,26 @@ $statement = $dbHandler->prepare($sql);
 $statement->bindParam(":id", $id);
 $statement->execute();
 
-$postId = $statement->fetchAll(PDO::FETCH_ASSOC);
-//dtata from db 
+$postRow = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-// sql statment that selects the number of rows in table posts
-$sql = "SELECT COUNT(*) FROM posts";
+// Sql statment that selects the number of rows in table comments.
+$sql = "SELECT COUNT(*) FROM comments";
 $statement = $dbHandler->prepare($sql);
 $statement->execute();
 echo "antal rader: " . $statement->fetchColumn() . "<br />";
 
-// Fetch all records in table posts and store them in an array
-$sql = "SELECT * FROM posts";
+// Fetch all records in table comments and store them in an array
+// $sql = "SELECT * FROM comments WHERE posts_id=:posts_id";
+// $statement->bindValue(":posts_id", $id);
+// $statement = $dbHandler->query($sql);
+// $statement->execute();
+
+$sql = "SELECT * FROM comments WHERE posts_id=:posts_id";
 $statement = $dbHandler->query($sql);
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$statement->bindParam(":posts_id", $id);
+$statement->execute();
+
+$comments = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -36,7 +43,7 @@ $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <body>
-    <?php foreach ($postId as $post) : ?>
+    <?php foreach ($postRow as $post) : ?>
         <h2><?= $post['title'] ?></h2>
         <p><?= $post['description'] ?></p>
         <a href="<?= $post['link'] ?>">This is link</a>
@@ -49,6 +56,14 @@ $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
         <textarea id="description" name="description" placeholder="Write something.." style="height:200px"></textarea>
         <button type="submit">Add comment</button>
     </form>
+
+    <?php foreach ($comments as $comment) :
+    ?><div>
+            <em><?= $comment['user']; ?> <?= $comment['time_stamp']; ?></em>
+            <p><?= $comment['description'];
+            endforeach; ?>
+            </p>
+        </div>
 </body>
 
 </html>
