a9ffbbb8544eb64d8e8f0fb43ca1d825055fce1c
rikardseg <70521759+rikardseg@users.noreply.github.com>
2021-01-04 18:57:27 +0100

    sort popular and newest

--- index.php
+++ index.php
@@ -2,14 +2,28 @@
 
 require __DIR__ . '/alwaysload.php';
 
+$sort_option = "";
+
+if (isset($_GET['sort_option'])) {
+  $sort_option = $_GET['sort_option'];
+}
+
 // sql statment that selects the number of rows in table posts
 $sql = "SELECT COUNT(*) FROM posts";
 $statement = $dbHandler->prepare($sql);
 $statement->execute();
 echo "antal rader: " . $statement->fetchColumn() . "<br />";
 
+// Fetch all records in table posts and store them in an array; sort according to $sort_order
+if ($sort_option === "time_stamp") {
+  $sql = "SELECT * FROM posts ORDER BY time_stamp DESC";
+} else if ($sort_option === "votes") {
+  $sql = "SELECT * FROM posts ORDER BY votes DESC";
+} else {
+  $sql = "SELECT * FROM posts";
+}
+echo "$sql";
 // Fetch all records in table posts and store them in an array
-$sql = "SELECT * FROM posts";
 $statement = $dbHandler->query($sql);
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
@@ -48,8 +62,8 @@ $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
   </header>
   <ul>
     <li><a href="newpost.php">Create new post</a></li>
-    <li><a href="#Most popular">Most popular</a></li>
-    <li><a href="#Newest posts">Newest posts</a></li>
+    <li><a href="index.php?sort_option=votes">Most popular</a></li>
+    <li><a href="index.php?sort_option=time_stamp">Newest posts</a></li>
     <li><a href="login.php">Login</a></li>
     <li><a href="logout.php">Logout</a></li>
     <li><a href="signup.php">Sign up</a></li>
